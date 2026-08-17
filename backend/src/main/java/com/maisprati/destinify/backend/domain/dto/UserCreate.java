55b87566bde39c9d627fb8d183d28b348275ee95
rafagalvaodev <rafagalvaonull@gmail.com>
2026-08-17 13:18:09 -0300

    :heavy_plus_sign: new attribute added

--- backend/src/main/java/com/maisprati/destinify/backend/domain/dto/UserCreate.java
+++ backend/src/main/java/com/maisprati/destinify/backend/domain/dto/UserCreate.java
@@ -1,9 +1,15 @@
 package com.maisprati.destinify.backend.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
 
 public record UserCreate(
         @NotBlank String name,
         @NotBlank String email,
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        @NotNull LocalDate birthdate,
         @NotBlank String password) {
 }
